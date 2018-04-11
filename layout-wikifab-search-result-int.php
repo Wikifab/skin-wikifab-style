f83fb408fb105dbf0dc644c17259b5ce6039b702
Julien <julien@dokit.io>
2018-04-11 14:32:08 +0200

    568 - Traduire les propriétés des vignettes

--- layout-wikifab-search-result-int.php
+++ layout-wikifab-search-result-int.php
@@ -19,7 +19,13 @@
 							<?php echo $creator; ?>
 							<?php if (isset($Area)):?>
 							<span class="project-byline-separator">|</span>
-							<span class="area-style"><?php echo $Area; ?></span>
+							<span class="area-style"><?php $values = explode(',', $Area);
+								$intValues = [];
+								$intKeyPrefix = 'wf-propertyvalue-area-';
+								foreach ($values as $v) {
+									$intValues[] = wfMessage( $intKeyPrefix . trim($v));
+								}
+								$intValue = implode(', ', $intValues); echo $intValue; ?></span>
 							<?php endif; ?>
 						</p>
 						<?php if (isset($Description)):?>
@@ -30,13 +36,13 @@
 								<?php if (isset($Difficulty)):?>
 									<li>
 									<i class="fa fa-briefcase"></i>
-									<span class="location-name"><?php echo $Difficulty; ?></span>
+									<span class="location-name"><?php echo wfMessage('wf-propertyvalue-difficulty-'.trim($Difficulty)); ?></span>
 									</li>
 								<?php endif; ?>
 								<?php if (isset($Business)):?>
 									<li>
 									<i class="fa fa-building-o"></i>
-									<span class="location-name"><?php echo $Business; ?></span>
+									<span class="location-name"><?php echo wfMessage('wf-propertyvalue-business-'.trim($Business)); ?></span>
 									</li>
 								<?php endif; ?>
 								<?php if (isset($Cost)):?>
@@ -48,7 +54,7 @@
 								<?php if (isset($Duration)):?>
 									<li class="last">
 									<i class="fa fa-hourglass-half"></i>
-									<span class="location-name"><?php echo $Duration; ?> <?php echo $Duration_type; ?></span>
+									<span class="location-name"><?php echo $Duration; ?> <?php echo wfMessage('wf-propertyvalue-durationtype-'.trim($Duration_type)); ?></span>
 									</li>
 								<?php endif; ?>
 							</ul>
